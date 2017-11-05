ad6d6692caaa564e3936d83a8b454660831e6c6a
Andrés Ávila <andaviaco@gmail.com>
2017-11-05 12:59:34 -0600

    implement initialization

--- src/tblo.py
+++ src/tblo.py
@@ -1,3 +1,6 @@
+import numpy as np
+import random as rand
+import pprint as pp
 
 
 class Learner(object):
@@ -5,28 +8,34 @@ class Learner(object):
     def __init__(self, initial_subjects, initial_fitness):
         super(Learner, self).__init__()
 
-        self.subjects = initial_solution
+        self.subjects = initial_subjects
         self.fitness = initial_fitness
 
     def __repr__(self):
-        pass
+        return f'<Learner s: {self.subjects} | f:{self.fitness} >'
 
 
 class TBLO(object):
+    learners = []
+
     def __init__(self, npopulation,  ngenerations, fn_eval, *, fn_lb, fn_ub):
         super(TBLO, self).__init__()
 
         self.npopulation = npopulation
         self.ngenerations = ngenerations
         self.fn_eval = fn_eval
-        self.fn_lb = fn_lb
-        self.fn_ub = fn_ub
+        self.fn_lb = np.array(fn_lb)
+        self.fn_ub = np.array(fn_ub)
+
 
     def optimize(self):
-        pass
+        self.initialize()
+        pp.pprint(self.learners)
+
+        return [None, None]
 
     def initialize(self):
-        pass
+        self.learners = [self.create_learner() for i in range(self.npopulation)]
 
     def teacher_phase(self):
         pass
@@ -37,11 +46,22 @@ class TBLO(object):
     def get_teacher(self):
         pass
 
-    def create_learner(self):
-        pass
-
     def random_learner_excluding(self, excluded_index):
         pass
 
+    def fitness(self, solution):
+        result = self.fn_eval(solution)
+
+        return np.around(result, decimals=4)
+
+    def create_learner(self):
+        solution = self.random_vector(self.fn_lb, self.fn_ub)
+        fitness = self.fitness(solution)
+
+        return Learner(solution, fitness)
+
     def random_vector(self, lb, ub):
-        pass
+        r = rand.random()
+        solution = lb + (ub - lb) * r
+
+        return np.around(solution, decimals=4)
